b9267c0df2e0475e607e3a6b76381b419309baef
Andre Amorim <andre@Admins-MBP.home>
2016-11-09 21:14:53 +0000

    Codility test

--- features/codility.feature
+++ features/codility.feature
@@ -1,4 +1,4 @@
-Feature: Api Exercise
+Feature: Codility
 
 # Background:
       
@@ -6,46 +6,13 @@ Feature: Api Exercise
 #     And I do something for all scenaries as example any login
 
 #Automated tests
- Scenario: Test verify id from posts
- When I send the method GET with id "1"
- Then I have to see the code "200" from json
- And json has to return the same id in the field id
+ Scenario: Test Codility 1
+ When create a map as n "4"
+ Then create two ships as s "1B 2C,2D 4D" 
+ And create hits as t "2B 2D 3D 4D 4A"
+ Then return quantities of sunken ships and hitten ships was not sunken
 
- Scenario: Test verify body from posts
- When I send the method GET with id "1"
- Then I have to see the code "200" from json
- And verify body if is not empty
-
-Scenario: Test verify name from comments
- When I send the method GET to comments with id "1" 
- Then I have to see the code "200" from json
- And verify all names if are not empty
-
- Scenario: Test verify email from comments
- When I send the method GET to comments with id "1"
- Then I have to see the code "200" from json
- And verify all emails if are not empty
-
- Scenario: Test call comments by user id and verify post id
- When I send the method GET with post id "5"
- Then I have to see the code "200" from json
- And if there is the same post id
-
-Scenario: Test call posts by userid and verify all fields if exist
- When I send the method GET with user id "1"
- Then I have to see the code "200" from json
- And if there is id
- And if there is title
- And if there is body
-
- Scenario: Test POST method to posts
- When I send the POST method
- Then I have to see the code "201" from json
- And any id of creation as response
-
- Scenario: Test DELETE method to posts
- When I send  DELETE method to id "5"
- Then I check if id is still there in posts
+ 
 
 
 
--- features/step_definitions/codility_exercise_steps.rb
+++ features/step_definitions/codility_exercise_steps.rb
@@ -1,78 +1,16 @@
-When(/^I send the method GET with id "(.*?)"$/) do |id|
-  @id = id
-  @api = Apijson.new
-  @api.send_get_byid(id)
-
+When(/^create a map as n "(.*?)"$/) do |n|
+  @n=n.to_i
 end
 
-Then(/^I have to see the code "(.*?)" from json$/) do |status_code|
-  @api.verify_status_code(status_code)
+Then(/^create two ships as s "(.*?)"$/) do |s|
+  @s=s
 end
 
-Then(/^json has to return the same id in the field id$/) do
-  @api.verify_id(@id)
+Then(/^create hits as t "(.*?)"$/) do |t|
+  @t=t
 end
 
-Then(/^verify body if is not empty$/) do
-   @api.verify_body_empty
-end
-
-When(/^I send the method GET to comments with id "(.*?)"$/) do |id|
-  @api = Apijson.new
-  @api.send_get_byid_comments(id)
-end
-
-Then(/^verify all names if are not empty$/) do
-  @api.verify_comments_name_empty
-end
-
-Then(/^verify all emails if are not empty$/) do
-  @api.verify_comments_email_empty
-end
-
-Then(/^if there is the same post id$/) do
-  @api.verify_if_is_the_same_postid(@post_id)
-end
-
-When(/^I send the method GET with post id "(.*?)"$/) do |post_id|
-  @post_id = post_id
-  @api = Apijson.new
-  @api.send_get_comments_bypostid(post_id)
-end
-
-When(/^I send the method GET with user id "(.*?)"$/) do |user_id|
-  @api = Apijson.new
-  @api.send_get_comments_byuserid(user_id)
-end
-
-Then(/^if there is id$/) do
-  @api.verify_id_exists
-end
-
-Then(/^if there is title$/) do
-  @api.verify_title_exists
-end
-
-Then(/^if there is body$/) do
-  @api.verify_body_exists
-end
-
-When(/^I send the POST method$/) do
-  @api = Apijson.new
-  @api.send_post_method
-end
-
-Then(/^any id of creation as response$/) do
-  @api.verify_id_exists
-end
-
-When(/^I send  DELETE method to id "(.*?)"$/) do |id|
-  @id = id
-  @api = Apijson.new
-  @api.send_delete_method(id)
-end
-
-Then(/^I check if id is still there in posts$/) do
-  @api.send_get_byid(@id)
-  @api.verify_delete_method(@id)
+Then(/^return quantities of sunken ships and hitten ships was not sunken$/) do
+  @codility=SolutionCodility.new
+  @codility.solution(@n,@s,@t)
 end
