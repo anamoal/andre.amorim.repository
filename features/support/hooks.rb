44243409c6291011b740cbc55e46c255a7a044f1
Andre Amorim <andre@Admins-MBP.home>
2016-11-09 21:17:12 +0000

    Codility test v2

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,20 +1,9 @@
-require 'watir-webdriver'
-require 'json'
-require 'net/http'
-require 'open-uri'
-require 'rubygems'
-require 'watir-webdriver/extensions/alerts'
-require 'base64'
-require 'openssl'
-require 'selenium-webdriver'
 
-
-include Selenium
 
 
 Before do
   
-$url_api = "http://jsonplaceholder.typicode.com"
+
 
 end
 
